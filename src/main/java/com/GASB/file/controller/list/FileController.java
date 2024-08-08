2d5383d58fc222efffa9e18374f2189aa03b9464
Yubin <qls773322@naver.com>
2024-08-08 17:12:41 +0900

    KAN-322 <message : RabbitMQ 적용>

--- src/main/java/com/GASB/file/controller/list/FileController.java
+++ src/main/java/com/GASB/file/controller/list/FileController.java
@@ -1,11 +1,13 @@
 package com.GASB.file.controller.list;
 
+import com.GASB.file.config.rabbitmq.RabbitMQProperties;
 import com.GASB.file.model.dto.request.OrgIdRequest;
 import com.GASB.file.model.dto.response.dashboard.FileDashboardDto;
 import com.GASB.file.model.dto.response.history.FileHistoryDto;
 import com.GASB.file.model.dto.response.list.ResponseDto;
 import com.GASB.file.service.dashboard.FileBoardReturnService;
 import com.GASB.file.service.history.FileHistoryService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +19,15 @@ public class FileController {
 
     private final FileBoardReturnService fileBoardReturnService;
     private final FileHistoryService fileHistoryService;
+    private final RabbitTemplate rabbitTemplate;
+    private final RabbitMQProperties properties;
 
     @Autowired
-    public FileController(FileBoardReturnService fileBoardReturnService, FileHistoryService fileHistoryService){
+    public FileController(FileBoardReturnService fileBoardReturnService, FileHistoryService fileHistoryService, RabbitTemplate rabbitTemplate, RabbitMQProperties properties){
         this.fileBoardReturnService = fileBoardReturnService;
         this.fileHistoryService = fileHistoryService;
+        this.rabbitTemplate = rabbitTemplate;
+        this.properties = properties;
     }
     @GetMapping
     public String hello(){
@@ -37,8 +43,9 @@ public class FileController {
 
     @GetMapping("/history")
     public ResponseDto<List<FileHistoryDto>> fileHistoryList(){
-        // long org_id = orgIdRequest.getOrgId(); @RequestBody OrgIdRequest orgIdRequest
+        //long orgId = orgIdRequest.getOrgId(); // @RequestBody OrgIdRequest orgIdRequest
         List<FileHistoryDto> fileHistory = fileHistoryService.historyListReturn();
+        // rabbitTemplate.convertAndSend(properties.getGroupingRoutingKey(),orgId);
         return ResponseDto.ofSuccess(fileHistory);
     }
 }
