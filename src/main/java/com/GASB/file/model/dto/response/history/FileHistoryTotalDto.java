8cc4daf75b22a334f572afdfddb9754e727c5fdf
Yubin <qls773322@naver.com>
2024-08-09 12:28:44 +0900

    KAN-335 <message : History Statistics API>

--- src/main/java/com/GASB/file/model/dto/response/history/FileHistoryTotalDto.java
+++ src/main/java/com/GASB/file/model/dto/response/history/FileHistoryTotalDto.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FileHistoryTotalDto {
     private int totalUpload;
     private int totalDeleted;
-    private int totalModify;
+    private int totalChanged;
     private int totalMoved;
     private List<FileHistoryStatistics> fileHistoryStatistics;
 }
