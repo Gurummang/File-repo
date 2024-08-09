9453948c40ab57ef0229c6e06ce7c88fcbf4a786
mandeuk <mandeuk1999@gmail.com>
2024-08-09 19:49:39 +0900

    KAN-336-group-name-refac / Feat: OrgRepo, for finding orgId, will delete

--- src/main/java/com/GASB/file/repository/org/OrgRepo.java
+++ src/main/java/com/GASB/file/repository/org/OrgRepo.java
@@ -1,2 +1,8 @@
-package com.GASB.file.repository.org;public interface OrgRepo {
+package com.GASB.file.repository.org;
+
+import com.GASB.file.model.entity.AdminUsers;
+import com.GASB.file.model.entity.Org;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrgRepo extends JpaRepository<Org, Integer> {
 }
