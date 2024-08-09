de9bff4fb6653f7687d25cde6eff2154ab7281f5
mandeuk <mandeuk1999@gmail.com>
2024-08-09 12:05:50 +0900

    KAN-321-grouping / Feat: Init Grouping function, for testing

--- src/main/java/com/GASB/file/controller/list/FileController.java
+++ src/main/java/com/GASB/file/controller/list/FileController.java
@@ -5,6 +5,7 @@ import com.GASB.file.model.dto.response.dashboard.FileDashboardDto;
 import com.GASB.file.model.dto.response.history.FileHistoryDto;
 import com.GASB.file.model.dto.response.list.ResponseDto;
 import com.GASB.file.service.dashboard.FileBoardReturnService;
+import com.GASB.file.service.history.FileGroupService;
 import com.GASB.file.service.history.FileHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +18,13 @@ public class FileController {
 
     private final FileBoardReturnService fileBoardReturnService;
     private final FileHistoryService fileHistoryService;
+    private final FileGroupService fileGroupService;
 
     @Autowired
-    public FileController(FileBoardReturnService fileBoardReturnService, FileHistoryService fileHistoryService){
+    public FileController(FileBoardReturnService fileBoardReturnService, FileHistoryService fileHistoryService, FileGroupService fileGroupService){
         this.fileBoardReturnService = fileBoardReturnService;
         this.fileHistoryService = fileHistoryService;
+        this.fileGroupService = fileGroupService;
     }
     @GetMapping
     public String hello(){
@@ -41,4 +44,7 @@ public class FileController {
         List<FileHistoryDto> fileHistory = fileHistoryService.historyListReturn();
         return ResponseDto.ofSuccess(fileHistory);
     }
+
+    @PostMapping("/grouping")
+    public void fileGrouping() { fileGroupService.groupFilesAndSave(); } // 그룹화만
 }
