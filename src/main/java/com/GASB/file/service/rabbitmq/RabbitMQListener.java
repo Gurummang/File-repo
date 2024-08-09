51e025fa5c6b5298493fb38110f1b5a95c5a6d2b
mandeuk <mandeuk1999@gmail.com>
2024-08-09 16:34:21 +0900

    KAN-336-group-name-refac / Feat: Listener call GroupingService

--- src/main/java/com/GASB/file/service/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/GASB/file/service/rabbitmq/RabbitMQListener.java
@@ -1,13 +1,19 @@
 package com.GASB.file.service.rabbitmq;
 
+import com.GASB.file.service.history.FileGroupService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQListener {
 
+    @Autowired
+    private FileGroupService fileGroupService;
+
     @RabbitListener(queues = "#{@rabbitMQProperties.groupingQueue}")
     public void onVtReportRequestReceived(long activitiesId) {
         System.out.println("received : " + activitiesId);
+        fileGroupService.groupFilesAndSave();
     }
 }
